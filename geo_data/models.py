1d088d55b78d4cc442af85f5d1f60796d2666f51
manjustice <manjustice1998@gmail.com>
2023-05-27 11:50:48 +0300

    Added model Place and created migrations

--- geo_data/models.py
+++ geo_data/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    geom = models.PointField()
