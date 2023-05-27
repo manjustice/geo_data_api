17e791832ab17b52184b807f8db3ed1a8eb3ad16
manjustice <manjustice1998@gmail.com>
2023-05-27 14:51:56 +0300

    Added documentation for api

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.gis",
     "rest_framework",
+    "drf_spectacular",
     "geo_data"
 ]
 
@@ -131,3 +132,20 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Geo data API",
+    "DESCRIPTION": "Geo data API",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        " ": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
--- geo_data/views.py
+++ geo_data/views.py
@@ -1,5 +1,7 @@
 from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import Point
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -11,8 +13,27 @@ class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
 
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "longitude",
+                type=OpenApiTypes.FLOAT,
+                description="Longitude of the point (ex. ?longitude=20.506235)",
+            ),
+            OpenApiParameter(
+                "latitude",
+                type=OpenApiTypes.FLOAT,
+                description="Latitude of the point (ex. ?latitude=50.046235)",
+            ),
+        ]
+    )
     @action(detail=False, methods=["GET"])
     def nearest_place(self, request):
+        """
+        Get nearest place to point.\n
+        Endpoint must take longitude and latitude in parameters.\n
+        Longitude and latitude must be float type
+        """
         longitude = request.GET.get("longitude")
         latitude = request.GET.get("latitude")
 
@@ -45,3 +66,47 @@ class PlaceViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(nearest_place)
 
         return Response(serializer.data)
+
+    def list(self, request, *args, **kwargs):
+        """Get all places in the database """
+        return super().list(self, request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Create place
+
+        Name: string
+
+        Description: string
+
+        Geom: string, format: Point('longitude', 'latitude') - longitude and latitude must be float type
+        """
+        return super().create(self, request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        """Get place information by its id"""
+        return super().retrieve(self, request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Update a place by its id
+
+        Name: string
+
+        Description: string
+
+        Geom: string, format: Point('longitude', 'latitude') - longitude and latitude must be float type
+        """
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+        Partial update of a place by its id
+
+        Name: string
+
+        Description: string
+
+        Geom: string, format: Point('longitude', 'latitude') - longitude and latitude must be float type
+        """
+        return super().partial_update(self, request, *args, **kwargs)
